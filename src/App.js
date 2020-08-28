e3606f5a9980f279964f8f71467536c1207a28ca
Dima Tsoy <dimktsoy@yahoo.com>
2020-08-28 12:21:44 +0600

    Add App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AddColorFormContainer from './components/AddColorForm/AddColorFormContainer';
+import ColorListContainer from './components/ColorList/ColorListContainer';
+import './App.scss';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <AddColorFormContainer />
+      <ColorListContainer />
     </div>
   );
-}
+};
 
 export default App;
