bc542d7c8ac6c99c3c2a4f10d1055601d9d9e906
Dima Tsoy <dimktsoy@yahoo.com>
2020-08-28 12:24:29 +0600

    Updates

---
+++ precache-manifest.2609151314becd770f3625f4cd2c6b15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf77f574faa6d42a82c45464b79a4ca",
+    "url": "/color-organizer/index.html"
+  },
+  {
+    "revision": "2ba9398a8fb0e29a2c70",
+    "url": "/color-organizer/static/css/main.466540f0.chunk.css"
+  },
+  {
+    "revision": "0e19d89c5951c84ae999",
+    "url": "/color-organizer/static/js/2.72ff6e68.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/color-organizer/static/js/2.72ff6e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ba9398a8fb0e29a2c70",
+    "url": "/color-organizer/static/js/main.6f37f1c6.chunk.js"
+  },
+  {
+    "revision": "88972359af549de0c9c1",
+    "url": "/color-organizer/static/js/runtime-main.430e8e47.js"
+  },
+  {
+    "revision": "91bebd82126223907cf0fc243f9668ad",
+    "url": "/color-organizer/static/media/color-icon-remove.91bebd82.svg"
+  }
+]);
